521952e66e1a11e5b9f25113a0e7e21ec67ce225
Lakshmi Manjeesh <lakshmimanjeesh123@gmail.com>
2026-02-21 12:39:23 +0530

    save changes

--- backend/server.js
+++ backend/server.js
@@ -5,60 +5,58 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 🎵 Mood Database (3 songs + 1 instrumental each)
 const moodSongs = {
 
   happy: [
-    { name: "Happy", artist: "Pharrell Williams", link: "https://youtu.be/ZbZSe6N_BXs" },
-    { name: "On Top of the World", artist: "Imagine Dragons", link: "https://youtu.be/w5tWYmIOWGk" },
-    { name: "Good Time", artist: "Owl City", link: "https://youtu.be/H7HmzwI67ec" },
-    { name: "A New Beginning (Instrumental)", artist: "Bensound", link: "https://youtu.be/6J8G5d8y7E0" }
+    { name: "Happy", artist: "Pharrell Williams", link: "https://open.spotify.com/track/60nZcImufyMA1MKQY3dcCH" },
+    { name: "On Top of the World", artist: "Imagine Dragons", link: "https://open.spotify.com/track/1BxfuPKGuaTgP7aM0Bbdwr" },
+    { name: "Good Time", artist: "Owl City", link: "https://open.spotify.com/track/3ZFTkvIE7kyPt6Nu3PEa7V" },
+    { name: "Sunset Lover (Instrumental Feel)", artist: "Petit Biscuit", link: "https://open.spotify.com/track/0L7LO1H39r3E6Udxj4xU0V" }
   ],
 
   sad: [
-    { name: "Let Her Go", artist: "Passenger", link: "https://youtu.be/RBumgq5yVrA" },
-    { name: "Fix You", artist: "Coldplay", link: "https://youtu.be/k4V3Mo61fJM" },
-    { name: "Someone Like You", artist: "Adele", link: "https://youtu.be/hLQl3WQQoQ0" },
-    { name: "Sad Piano (Instrumental)", artist: "Audio Library", link: "https://youtu.be/7maJOI3QMu0" }
+    { name: "Someone Like You", artist: "Adele", link: "https://open.spotify.com/track/4kflIGfjdZJW4ot2ioixTB" },
+    { name: "Fix You", artist: "Coldplay", link: "https://open.spotify.com/track/7LVHVU3tWfcxj5aiPFEW4Q" },
+    { name: "Let Her Go", artist: "Passenger", link: "https://open.spotify.com/track/2jyjW7sLisrN0xF7iBj2Yp" },
+    { name: "River Flows In You (Instrumental)", artist: "Yiruma", link: "https://open.spotify.com/track/2uU6Z2n8hXnq8oZ8kXq7A3" }
   ],
 
   stressed: [
-    { name: "Photograph", artist: "Ed Sheeran", link: "https://youtu.be/nSDgHBxUbVQ" },
-    { name: "Stay", artist: "Rihanna", link: "https://youtu.be/JF8BRvqGCNs" },
-    { name: "Skinny Love", artist: "Birdy", link: "https://youtu.be/aNzCDt2eidg" },
-    { name: "Deep Relaxation (Instrumental)", artist: "Meditative Mind", link: "https://youtu.be/lFcSrYw-ARY" }
+    { name: "Photograph", artist: "Ed Sheeran", link: "https://open.spotify.com/track/1HNkqx9Ahdgi1Ixy2xkKkL" },
+    { name: "Stay", artist: "Rihanna", link: "https://open.spotify.com/track/5VnDkUNyX6u5Sk0yZiP8XB" },
+    { name: "Skinny Love", artist: "Birdy", link: "https://open.spotify.com/track/3HfB5hBU0dmBt8T0iCmH42" },
+    { name: "Weightless (Instrumental Relaxation)", artist: "Marconi Union", link: "https://open.spotify.com/track/6kkwzB6hXLIONkEk9JciA6" }
   ],
 
   excited: [
-    { name: "Believer", artist: "Imagine Dragons", link: "https://youtu.be/7wtfhZwyrcc" },
-    { name: "Can't Stop The Feeling", artist: "Justin Timberlake", link: "https://youtu.be/ru0K8uYEZWw" },
-    { name: "Titanium", artist: "David Guetta", link: "https://youtu.be/JRfuAukYTKg" },
-    { name: "Epic Motivation (Instrumental)", artist: "Audio Library", link: "https://youtu.be/kz6n1kz3F0s" }
+    { name: "Believer", artist: "Imagine Dragons", link: "https://open.spotify.com/track/0pqnGHJpmpxLKifKRmU6WP" },
+    { name: "Titanium", artist: "David Guetta", link: "https://open.spotify.com/track/6b5P51m8xx2XA6U7sdNZ5E" },
+    { name: "Can't Stop The Feeling", artist: "Justin Timberlake", link: "https://open.spotify.com/track/6JV2JOEocMgcZxYSZelKcc" },
+    { name: "Levels (Instrumental Energy)", artist: "Avicii", link: "https://open.spotify.com/track/2V65y3PX4DkRhy1djlxd9p" }
   ],
 
   calm: [
-    { name: "Ocean Eyes", artist: "Billie Eilish", link: "https://youtu.be/viimfQi_pUw" },
-    { name: "Yellow", artist: "Coldplay", link: "https://youtu.be/yKNxeF4KMsY" },
-    { name: "Holocene", artist: "Bon Iver", link: "https://youtu.be/TWcyIpul8OE" },
-    { name: "Peaceful Guitar (Instrumental)", artist: "Bensound", link: "https://youtu.be/2OEL4P1Rz04" }
+    { name: "Ocean Eyes", artist: "Billie Eilish", link: "https://open.spotify.com/track/7hDVYcQq6MxkdJGweuCtl9" },
+    { name: "Yellow", artist: "Coldplay", link: "https://open.spotify.com/track/3AJwUDP919kvQ9QcozQPxg" },
+    { name: "Holocene", artist: "Bon Iver", link: "https://open.spotify.com/track/2X485T9Z5Ly0xyaghN73ed" },
+    { name: "Experience (Instrumental)", artist: "Ludovico Einaudi", link: "https://open.spotify.com/track/1BncfTJAWxrsxyT9culBrj" }
   ],
 
   romantic: [
-    { name: "Perfect", artist: "Ed Sheeran", link: "https://youtu.be/2Vv-BfVoq4g" },
-    { name: "All of Me", artist: "John Legend", link: "https://youtu.be/450p7goxZqg" },
-    { name: "Just the Way You Are", artist: "Bruno Mars", link: "https://youtu.be/LjhCEhWiKXk" },
-    { name: "Romantic Piano (Instrumental)", artist: "Audio Library", link: "https://youtu.be/9Q634rbsypE" }
+    { name: "Perfect", artist: "Ed Sheeran", link: "https://open.spotify.com/track/0tgVpDi06FyKpA1z0VMD4v" },
+    { name: "All of Me", artist: "John Legend", link: "https://open.spotify.com/track/3U4isOIWM3VvDubwSI3y7a" },
+    { name: "Just the Way You Are", artist: "Bruno Mars", link: "https://open.spotify.com/track/7BqBn9nzAq8spo5e7cZ0dJ" },
+    { name: "Comptine d’un autre été (Instrumental)", artist: "Yann Tiersen", link: "https://open.spotify.com/track/6e6hG6E3VnAhEo1mKzOe1b" }
   ],
 
   angry: [
-    { name: "Numb", artist: "Linkin Park", link: "https://youtu.be/kXYiU_JCYtU" },
-    { name: "Stronger", artist: "Kanye West", link: "https://youtu.be/PsO6ZnUZI0g" },
-    { name: "Centuries", artist: "Fall Out Boy", link: "https://youtu.be/LBr7kECsjcQ" },
-    { name: "Dark Energy (Instrumental)", artist: "Audio Library", link: "https://youtu.be/DWcJFNfaw9c" }
+    { name: "Numb", artist: "Linkin Park", link: "https://open.spotify.com/track/2nLtzopw4rPReszdYBJU6h" },
+    { name: "Stronger", artist: "Kanye West", link: "https://open.spotify.com/track/4fzsfWzRhPawzqhX8Qt9F3" },
+    { name: "Centuries", artist: "Fall Out Boy", link: "https://open.spotify.com/track/6U1e9zY9M7oF5mT1Qv1h5W" },
+    { name: "Time (Instrumental)", artist: "Hans Zimmer", link: "https://open.spotify.com/track/1lDWb6b6ieDQ2xT7ewTC3G" }
   ]
 };
 
-// 🧠 Mood Calculation
 app.post("/analyze", (req, res) => {
   const { answers } = req.body;
   const score = answers.reduce((a, b) => a + b, 0);
@@ -76,4 +74,6 @@ app.post("/analyze", (req, res) => {
   res.json({ mood, songs: moodSongs[mood] });
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,39 @@
-let answers = [];
+let answers = [0, 0, 0];
 
-function add(val) {
-  answers.push(val);
+function add(value, index) {
+  answers[index] = value;
 }
 
 async function submitQuiz() {
-  const res = await fetch("http://localhost:3000/analyze", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ answers })
-  });
 
-  const data = await res.json();
+  if (answers.includes(0)) {
+    alert("Please answer all questions!");
+    return;
+  }
 
-  const result = document.getElementById("result");
-  result.innerHTML = `<h2>Mood Detected: ${data.mood}</h2>`;
+  try {
+    const response = await fetch("http://localhost:3000/analyze", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ answers })
+    });
 
-  data.songs.forEach(song => {
-    result.innerHTML += `
-      <div class="song">
-        <b>${song.name}</b><br>
-        ${song.artist}<br>
-        <a href="${song.link}" target="_blank">▶ Play</a>
-      </div>
-    `;
-  });
+    const data = await response.json();
 
-  answers = [];
+    const result = document.getElementById("result");
+    result.innerHTML = `<h2>Mood Detected: ${data.mood.toUpperCase()}</h2>`;
+
+    data.songs.forEach(song => {
+      result.innerHTML += `
+        <div class="song">
+          <b>${song.name}</b><br>
+          ${song.artist}<br>
+          <a href="${song.link}" target="_blank">▶ Play</a>
+        </div>
+      `;
+    });
+
+  } catch (error) {
+    alert("Backend not running!");
+  }
 }
